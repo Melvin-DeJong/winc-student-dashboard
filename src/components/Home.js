f8610067cada868aae9329e67f18ca563c821c71
Melvin de Jong <melvindejong22@gmail.com>
2020-07-24 17:00:35 +0200

    update home page
    updated the homepage with logo, and some text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
+import WincLogo from "../assets/logo_winc.jpg";
 
 export default () => 
-<div>
+<div className="home">
+    
+    <img id="logoWinc" src={WincLogo} alt="winclogo" />
     <h1>
-        Home!
+        Welcome!
     </h1>
+    <h2>
+        Winc academy Student Dashboard
+    </h2>
+    <h3>
+        With the help of this dashboard you can see the results of the students.
+    </h3>
+    <h3>
+        Need more info or contact details of a student? Then take a look at the student page
+    </h3>
+
+
 </div>;
